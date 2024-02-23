181adcaf4ecc9a7a228310c3114e01abe9063f78
Baibhav <baibhavtiwari37@gmail.com>
2024-02-23 19:06:17 +0530

    fix_f1

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
+import { SignUp } from '@clerk/nextjs'
+
 const SignUpPage = () => {
-  return (
-    <div>SignUp</div>
-  )
+  return <SignUp />
 }
 
 export default SignUpPage
