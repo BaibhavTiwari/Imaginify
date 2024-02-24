e8acdb682d4ded65539c0bfe3198222823ae4d84
Baibhav <baibhavtiwari37@gmail.com>
2024-02-24 16:00:17 +0530

    Created Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+import Sidebar from '@/components/shared/Sidebar'
+
+function Layout({ children }: { children: React.ReactNode; }) {
   return (
     <main className="root">
+      <Sidebar />
       <div className='root-container'>
         <div className='wrapper'>
           {children}
         </div>
       </div>
     </main>
-  )
+  );
 }
 
 export default Layout
